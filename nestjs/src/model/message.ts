8e770b42b1f9a430006846c76323e903c3ec2958
Vouts <machevoutsa@gmail.com>
2025-06-02 16:03:44 +0200

    creation du modele message

---
+++ nestjs/src/model/message.ts
@@ -0,0 +1,32 @@
+import {ObjectType, Field, ID} from '@nestjs/graphql';
+import {User} from './user';
+import {Conversation} from './conversation';
+
+@ObjectType()
+export class Message {
+    @Field(() => ID)
+    id: string;
+
+    @Field()
+    content: string;
+
+    @Field()
+    senderId: string;
+
+    @Field(()=> User, { nullable: true })
+    sender?: User;
+
+    @Field()
+    conversationId: string;
+
+    
+
+    
+
+    @Field(() => Conversation, { nullable: true })
+    conversation?: Conversation;
+
+    @Field()
+    createdAt: Date;
+
+}
